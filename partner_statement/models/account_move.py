b6cd815e501adb66fd1925d5c3d10944c401d538
akradore <akradore@gmail.com>
2021-04-22 09:04:33 +0200

    [ADD] Adding Partner Statement

---
+++ partner_statement/models/account_move.py
@@ -0,0 +1,31 @@
+# Copyright 2018 ForgeFlow, S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+import logging
+
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    account_internal_type = fields.Selection(related='account_id.user_type_id.type', string="Internal Type",
+                                             readonly=True, store=True)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    customer = fields.Boolean(string='Customer' , default=True)
+    statement_sent = fields.Boolean('Statement Sent', default=False)
+    excl_fully_allocated_invoices = fields.Boolean(string="Exclude Fully Allocated Invoices")
+    statement_period = fields.Selection([('current_month', 'Current Month'),
+                                         ('current_quarter', 'Current Quarter'),
+                                         ('current_fiscal_year', 'Current Fiscal Year'),
+                                         ('last_fiscal_year', 'Last Fiscal Year'),
+                                         ('last_quarter', 'Last Quarter'),
+                                         ('last_month', 'Last Month'), ], default="current_month",
+                                        string="Statement Period")
