a9912a5d59d8afe56518fcfcea5b7f58b217bd64
Vishal <vishal@strategicdimensions.co.za>
2021-04-26 17:09:53 +0530

    [update code for auto populate project_id in new quotation]

---
+++ base_pinnacle/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Pinnacle",
+    'summary': """""",
+    'description': """""",
+    'category': 'Uncategorized',
+    'version': '14.0.1.0',
+    'depends': [
+        'base', 'web', 'crm', 'sale'
+    ],
+    'data': [
+        'views/crm_lead_view.xml'
+    ],
+    # 'qweb': [
+    #     'static/src/xml/subscription_report_template.xml',
+    # ],
+}
---
+++ base_pinnacle/models/crm_lead.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    project_id = fields.Many2one('project.project', string='Project')
+
+    def action_sale_quotations_new(self):
+        if not self.partner_id:
+            return self.env["ir.actions.actions"]._for_xml_id("sale_crm.crm_quotation_partner_action")
+        else:
+            return self.action_new_quotation()
+
+    def compute_action_new_quotation(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("sale_crm.sale_action_quotations_new")
+        action['context'] = {
+            'search_default_opportunity_id': self.id,
+            'default_opportunity_id': self.id,
+            'search_default_partner_id': self.partner_id.id,
+            'default_partner_id': self.partner_id.id,
+            'default_team_id': self.team_id.id,
+            'default_campaign_id': self.campaign_id.id,
+            'default_medium_id': self.medium_id.id,
+            'default_origin': self.name,
+            'default_source_id': self.source_id.id,
+            'default_company_id': self.company_id.id or self.env.company.id,
+            'default_tag_ids': [(6, 0, self.tag_ids.ids)],
+            'default_analytic_account_id': self.project_id.analytic_account_id.id
+        }
+        # print("\n\n====action", action)
+        return action
+
