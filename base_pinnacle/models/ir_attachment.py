062f1b847f380c5b58cfc05cd247752532b84823
akradore <akradore@gmail.com>
2021-04-28 09:40:19 +0200

    [FIX] Made changes to attachments

--- base_pinnacle/models/ir_attachment.py
+++ base_pinnacle/models/ir_attachment.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, _
+from odoo import api, models, fields, _
 
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
+    @api.model
     def create(self, vals):
-        if vals['res_model'] == 'crm.lead':
-            crm_id = self.env['crm.lead'].browse(vals['res_id'])
+        if vals.get('res_model') == 'crm.lead':
+            crm_id = self.env['crm.lead'].browse(vals.get('res_id'))
             if crm_id:
                 self.create({
                     'res_model': 'project.project',
                     'res_id': crm_id.project_id.id,
-                    'datas': vals['datas'],
-                    'name': vals['name'],
+                    'datas': vals.get('datas'),
+                    'name': vals.get('name'),
                 })
-        elif vals['res_model'] == 'purchase.order':
-            purchase_order_id = self.env['purchase.order'].browse(vals['res_id'])
+        elif vals.get('res_model') == 'purchase.order':
+            purchase_order_id = self.env['purchase.order'].browse(vals.get('res_id'))
             if purchase_order_id:
                 for each in purchase_order_id.order_line:
                     self.create({
-                        'res_model': 'product.product',
-                        'res_id': each.product_id.id,
-                        'datas': vals['datas'],
-                        'name': vals['name'],
+                        'res_model': 'account.analytic.account',
+                        'res_id': each.analytic_account_id.id,
+                        'datas': vals.get('datas'),
+                        'name': vals.get('name'),
                     })
         return super(IrAttachment, self).create(vals)
 
